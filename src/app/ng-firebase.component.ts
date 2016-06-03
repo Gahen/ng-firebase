73c898e74defcaf0ad1eb5bae737504cfb3cadc9
Joaquín Omar Tumas <joaquin@kidloom.com>
2016-06-02 21:27:17 -0300

    testing adding a new ng route

--- src/app/ng-firebase.component.ts
+++ src/app/ng-firebase.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { DashboardComponent } from './+dashboard';
+import { Routes , ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from '@angular/router';
 
 @Component({
   moduleId: module.id,
   selector: 'ng-firebase-app',
   templateUrl: 'ng-firebase.component.html',
-  styleUrls: ['ng-firebase.component.css']
+  styleUrls: ['ng-firebase.component.css'],
+  directives: [ROUTER_DIRECTIVES],
+  providers: [ROUTER_PROVIDERS]
 })
+@Routes([
+  {path: '/dashboard', component: DashboardComponent}
+])
 export class NgFirebaseAppComponent {
   title = 'ng-firebase works!';
 }
